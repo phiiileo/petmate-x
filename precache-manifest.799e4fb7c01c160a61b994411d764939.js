cfd9397db08ef776b34ccf46695acbc6c4d94807
phiiileo <emmanuelboluwajib1@gmail.com>
2020-01-07 04:41:44 +0100

    Updates

---
+++ precache-manifest.799e4fb7c01c160a61b994411d764939.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14039d2aa85a011435813e019e2ec4e",
+    "url": "/petmate-x/index.html"
+  },
+  {
+    "revision": "cc72038d0ce25d1a8776",
+    "url": "/petmate-x/static/css/main.f312ce77.chunk.css"
+  },
+  {
+    "revision": "c7c446fead7b20d094a4",
+    "url": "/petmate-x/static/js/2.b9eb801d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/petmate-x/static/js/2.b9eb801d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc72038d0ce25d1a8776",
+    "url": "/petmate-x/static/js/main.c2c4cb7f.chunk.js"
+  },
+  {
+    "revision": "4f0e114f0b69b75cb04b",
+    "url": "/petmate-x/static/js/runtime-main.c776a8b0.js"
+  }
+]);
