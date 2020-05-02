043cd9e30f7e49124f1bd43488a7d09f8b47afd1
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-05-03 00:57:54 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom"
-import './sass/App.scss';
 import LandingPage from './Components/LandingPage/LandingPage';
 
 class App extends React.Component {
